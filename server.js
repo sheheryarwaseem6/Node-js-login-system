aad7036a78d9047d16b4e775ea213e51b180aa80
Sheheryar Waseem <sherishaikh6@gmail.com>
2022-01-23 17:07:33 +0500

    updated

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt')
 const app = express()
 
+let users = []
+
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended : false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -13,14 +17,31 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login',(req, res) => {
-    
+
 })
 
 app.get('/register', (req, res) =>{
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
+    try{
+        const hashedPass = await bcrypt.hash(req.body.password , 10)
+        users.push({
+            id : Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPass
+
+        })
+        res.redirect('/login')
+
+    }catch{
+        res.redirect('/register')
+
+    }
+    console.log(users)
+
 
 })
 
